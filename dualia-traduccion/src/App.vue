2ad851b96d13f3d0e46a36a3795d61334967b7fe
am4rtinez <anglxd@gmail.com>
2022-06-22 15:18:14 +0200

    Pruebas VUE

---
+++ dualia-traduccion/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+	import Sidebar from './components/sidebar/Sidebar.vue'
+	export default {
+		components: { Sidebar }
+	}
+</script>
+
+<template>
+	<Sidebar />
+	<div>
+		<router-view />
+	</div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#logo{
+	width: 40%;
+}
+</style>
---
+++ dualia-traduccion/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// require('./plugins/fontawesome');
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+createApp(App)
+.use(router)
+.component('fa', FontAwesomeIcon)
+.mount('#app')
